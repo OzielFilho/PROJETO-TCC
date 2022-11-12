1c22a346a64c117aa9830f634448aaff6510d1ee
OzielFilho <ozielfilho03@hotmail.com>
2022-11-12 00:40:16 -0300

    refactor : change message of current location user

--- lib/app/core/utils/functions_utils.dart
+++ lib/app/core/utils/functions_utils.dart
@@ -3,7 +3,7 @@ import '../../modules/home/domain/entities/current_position.dart';
 
 class FunctionUtils {
   static String currentLocationMessage(CurrentPosition position) {
-    return 'Olá, minha localização atual é https://www.google.com/maps/place/${position.lat},${position.long}';
+    return 'Socorro, minha localização atual é https://www.google.com/maps/place/${position.lat},${position.long}';
   }
 
   static Future<CurrentPosition> getLocation() async {

9b7d5b47ba0ae2045ed3a6ef67e2d3096ee7b444
OzielFilho <ozielfilho03@hotmail.com>
2023-01-27 14:52:42 -0300

    verification internet in recovery password

--- test/modules/authentication/recovery_password/interactor/recovery_password_interactor_executor_test.dart
+++ test/modules/authentication/recovery_password/interactor/recovery_password_interactor_executor_test.dart
@@ -4,6 +4,8 @@ import 'package:test/test.dart';
 
 import '../../../../mocks/services/firebase_auth_service_mock_200.dart';
 import '../../../../mocks/services/firebase_auth_service_mock_exception.dart';
+import '../../../../mocks/services/network_service_mock_200.dart';
+import '../../../../mocks/services/network_service_mock_exception.dart';
 import '../presenter/recovery_password_presenter_listener_mock.dart';
 
 void main() {
@@ -14,11 +16,11 @@ void main() {
   group('Recovery Password Executor Test', () {
     test('Recovery Password - Interactor - Success', () async {
       _authService = FirebaseAuthServiceMock200(mock: 'success');
-
+      final networkService = NetworkServiceMock200();
       final recoveryPassword = RecoveryPasswordInteractorExecutor(
-        listener: _recoveryListener,
-        authService: _authService,
-      );
+          listener: _recoveryListener,
+          authService: _authService,
+          networkService: networkService);
 
       await recoveryPassword.recoveryPassword(_email);
 
@@ -27,10 +29,11 @@ void main() {
 
     test('Recovery Password - Interactor - Failure', () async {
       _authService = FirebaseAuthServiceMockException();
+      final networkService = NetworkServiceMockException();
       final recoveryPassword = RecoveryPasswordInteractorExecutor(
-        listener: _recoveryListener,
-        authService: _authService,
-      );
+          listener: _recoveryListener,
+          authService: _authService,
+          networkService: networkService);
 
       await recoveryPassword.recoveryPassword(_email);
 
